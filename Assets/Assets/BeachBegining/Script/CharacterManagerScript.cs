c3b101771b288afa4a5a550d8532c6687708e2ab
Esteban5555 <al361641@uji.es>
2020-12-15 20:22:36 +0100

    Ruber Ring and water mechanics functional

---
+++ Assets/Assets/BeachBegining/Script/CharacterManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharacterManagerScript : MonoBehaviour
+{
+    bool PlayerWithFlotador = false;
+    private GameObject Grid;
+    private Transform Agua;
+    private GameObject Player;
+    private MainCharacter mainCharacterScript;
+    private TilemapCollider2D WaterCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid = GameObject.Find("Grid");
+        Agua = Grid.transform.Find("Agua");
+        WaterCollider = Agua.GetComponent<TilemapCollider2D>();
+        Player = GameObject.Find("MainCharacter");
+
+        mainCharacterScript = Player.GetComponent <MainCharacter>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (mainCharacterScript.RubberRing == true)
+        {
+            PlayerWithFlotador = true;
+        }
+        else { PlayerWithFlotador = false; }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerWithFlotador && WaterCollider != null)
+        {
+            WaterCollider.enabled = false;
+        }
+        else { WaterCollider.enabled = true; }
+    }
+}
