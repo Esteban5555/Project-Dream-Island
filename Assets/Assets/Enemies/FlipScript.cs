f60035a84f62adf83a7234bef8e0c58a1293e022
Esteban5555 <al361641@uji.es>
2020-12-19 11:25:04 +0100

    Grid and Pathfinding

---
+++ Assets/Assets/Enemies/FlipScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class FlipScript : MonoBehaviour
+{
+    public AIPath aipath;
+    bool facingRight = true;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aipath.desiredVelocity.x > 0 && facingRight == false)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            facingRight = !facingRight;
+        }
+        else { if (aipath.desiredVelocity.x < 0 && facingRight) {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                facingRight = !facingRight;
+            } 
+        }
+    }
+}
---
+++ Assets/Assets/GeneralScripts/GridGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridGenerator : MonoBehaviour
+{
+    public Tilemap WalkableTilemap;
+    public Vector3Int[,] cells;
+    BoundsInt bounds;
+    Astar astar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WalkableTilemap.CompressBounds();
+        bounds = WalkableTilemap.cellBounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreatGrid() {
+        cells = new Vector3Int[bounds.size.x, bounds.size.y];
+
+        for (int x = bounds.xMin, i = 0; i < (bounds.size.x); x++, i++)
+        {
+            for (int y = bounds.yMin, j = 0; j < (bounds.size.y); y++, j++)
+            {
+                if (WalkableTilemap.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    cells[i, j] = new Vector3Int(x, y, 0);
+                }
+                else
+                {
+                    cells[i, j] = new Vector3Int(x, y, 1);
+                }
+            }
+        }
+    }
+}
