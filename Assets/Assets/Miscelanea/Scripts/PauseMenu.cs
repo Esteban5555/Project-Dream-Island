fe6ec0e06eb33ec14a2759815d37b5ea0317fae4
Esteban5555 <al361641@uji.es>
2021-02-02 21:59:14 +0100

    simplePauseMenu

---
+++ Assets/Assets/Miscelanea/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu = GameObject.Find("Menu");
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Quit() { 
+    
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
