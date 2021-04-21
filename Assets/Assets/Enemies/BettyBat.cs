aefbf33724712ace5acbfb4d972d2f284deeae29
Esteban5555 <al361641@uji.es>
2021-04-21 19:47:34 +0100

    BettyBat Enemy

---
+++ Assets/Assets/Enemies/BettyBat.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BettyBat : MonoBehaviour
+{
+    public int Health;
+    public int MaxHealth = 2;
+
+
+    public float maxInmunityTime = 0.5f;
+    public float inmunityTime = 0f;
+
+    public float swordForce = 100f;
+
+    Rigidbody2D rb;
+    BettyBatAI BettyBatAIscript;
+    Transform splatSpawn;
+    public GameObject deathSplat;
+
+    GameObject Manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Manager = GameObject.Find("SceneManager");
+        BettyBatAIscript = GetComponent<BettyBatAI>();
+        Health = MaxHealth;
+        splatSpawn = transform.Find("SplatSpawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            GameObject splat = Instantiate(deathSplat, splatSpawn);
+            splat.transform.parent = this.gameObject.transform.parent;
+            Destroy(this.gameObject);
+        }
+
+        if (inmunityTime < maxInmunityTime)
+        {
+            inmunityTime = inmunityTime + Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "SwordAtacks" && inmunityTime >= maxInmunityTime)
+        {
+            Vector2 force = (rb.transform.position - BettyBatAIscript.target.position).normalized * swordForce;
+            rb.AddForce(force);
+            Health = Health - Manager.GetComponent<CharacterManagerScript>().GetSwordAttackDamage();
+            inmunityTime = 0f;
+        }
+    }
+}
---
+++ Assets/Assets/Enemies/BettyBatAI.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class BettyBatAI : MonoBehaviour
+{
+    private enum enemyBettyBatStates
+    {
+        Hiding,
+        Follow,
+    }
+
+    private enemyBettyBatStates state;
+
+    public Transform target;
+
+    public float FollowSpeed = 200f;
+    public float speed = 5f;
+    public float nextWaypointDistance = 3f;
+    public float AwarnessRange = 5f;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    Animator anim;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    bool returning = false;
+
+    Vector3 roamPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+
+        state = enemyBettyBatStates.Hiding;
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            switch (state)
+            {
+                default:
+                case enemyBettyBatStates.Hiding:
+                    //seeker.StartPath(rb.position, roamPosition, OnPathComplete);
+                    anim.SetBool("Pursuing", false);
+                    //speed = RoamSpeed;
+                    break;
+
+                case enemyBettyBatStates.Follow:
+
+                    seeker.StartPath(rb.position, target.position, OnPathComplete);
+                    returning = false;
+                    speed = FollowSpeed;
+                    break;
+            }
+
+        }
+    }
+
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    private void Update()
+    {
+
+        if (Vector3.Distance(transform.position, target.position) < AwarnessRange)
+        {
+            anim.SetBool("Pursuing", true);
+            state = enemyBettyBatStates.Follow;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        //Move Enemy throuhout the path
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
