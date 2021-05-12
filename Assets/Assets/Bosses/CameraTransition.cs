cdca6fe02b5e11bc304de8d60757fb2a3ef8e5af
Esteban5555 <al361641@uji.es>
2021-05-12 21:31:00 +0100

    Camera Transitions in dungeon

---
+++ Assets/Assets/Bosses/CameraTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public GameObject FirstCeraBound, SecondCameraBound;
+
+   
+    
+    bool first = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.name == "MainCharacter") {
+
+            Debug.Log("ChangingCamera");
+            FirstCeraBound.SetActive(false);
+            SecondCameraBound.SetActive(false);
+
+            if (first) {
+                SecondCameraBound.SetActive(true);
+            }
+            else {
+                FirstCeraBound.SetActive(true);
+            }
+
+            first = !first;
+        }
+    }
+}
