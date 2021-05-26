10d5ea622be48c30efb95762b767c8c79381eae5
Esteban5555 <al361641@uji.es>
2021-05-26 20:45:25 +0100

    Door and levers

---
+++ Assets/Assets/Bosses/DoorManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public GameObject DoorClosed;
+
+    bool lever01, lever02 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoorClosed.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lever01 && lever02) {
+            DoorClosed.SetActive(false);
+        }
+    }
+
+    public void setLever() {
+        if (!lever01)
+        {
+            lever01 = true;
+
+        }
+        else {
+            lever02 = true;
+        }
+    }
+
+}
---
+++ Assets/Assets/Bosses/Doors/LeverScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    bool playerInRange = false;
+
+    private DoorManager manager;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("SceneManager").GetComponent<DoorManager>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && Input.GetButtonDown("Submit")) {
+            manager.setLever();
+            anim.SetBool("pressed", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "MainCharacter")
+        {
+            playerInRange = true;
+            Debug.Log("PlayerInRange");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "MainCharacter")
+        {
+            playerInRange = false;
+            Debug.Log("NotPlayerInRange");
+        }
+    }
+}
